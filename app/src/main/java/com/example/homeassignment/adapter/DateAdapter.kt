f0ab08e17eafe5890cc0858a173d706bb04433c2
Tauqeer Majeed <tauqeermajeed008@gmail.com>
2021-10-12 17:56:00 +0500

    initial commit

---
+++ app/src/main/java/com/example/homeassignment/adapter/DateAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.homeassignment.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.homeassignment.model.DateModel
+import com.example.homeassignment.R
+
+class DateAdapter (
+    private val context: Context,
+    private var dataList: ArrayList<DateModel>,
+):
+
+    RecyclerView.Adapter<DateAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.date_item_list,
+                null
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DateAdapter.ViewHolder, position: Int) {
+        with(holder)
+        {
+            val itemAtPos = dataList[position]
+            date?.text = itemAtPos.date
+            itemListRv.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            val itemListAdapter =
+                ItemAdapter(
+                    context,
+                    itemAtPos.itemList
+                )
+            itemListRv.adapter = itemListAdapter
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var date: TextView? = itemView.findViewById(R.id.date)
+        var itemListRv: RecyclerView = itemView.findViewById(R.id.nestedRv)
+    }
+}
---
+++ app/src/main/java/com/example/homeassignment/model/ItemModel.kt
@@ -0,0 +1,6 @@
+package com.example.homeassignment.model
+
+data class ItemModel (
+    var item: String? = null,
+    var price: Int? = -1
+)
